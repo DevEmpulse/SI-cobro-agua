3971ecfed8949c71baed38346577f25c474e0e1e
Agustin <esparta.01.af@gmail.com>
2025-05-08 19:10:55 -0300

    Fix

--- src/features/facturas/columns/Columns.tsx
+++ src/features/facturas/columns/Columns.tsx
@@ -1,46 +1,40 @@
-import { Button } from "@/components/ui/button"
-import { type ColumnDef } from "@tanstack/react-table"
+import { Button } from "@/components/ui/button";
+import { type ColumnDef } from "@tanstack/react-table";
+import type { Factura } from "@/types/factura";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
-export type Payment = {
-    id: number
-    numeroFactura: string
-    monto: string
-    fecha: Date
-}
 
-export const columns: ColumnDef<Payment>[] = [
-    {
-        accessorKey: "numeroFactura",
-        header: "Factura",
+export const columns: ColumnDef<Factura>[] = [
+  {
+    accessorKey: "numero",
+    header: "Factura",
+  },
+  {
+    accessorKey: "fecha",
+    header: "Fecha",
+  },
+  {
+    accessorKey: "Importe",
+    header: "Importe",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const payment = row.original;
+
+      return (
+        <div className="flex space-x-2">
+          <Button
+            className="cursor-pointer"
+            variant="destructive"
+            size="sm"
+            onClick={() => console.log("Borrar", payment.id)}
+          >
+            Borrar
+          </Button>
+        </div>
+      );
     },
-    {
-        accessorKey: "fecha",
-        header: "Fecha"
-    },
-    {
-        accessorKey: "monto",
-        header: "Monto",
-    },      
-    {
-        id: "actions",
-        cell: ({ row }) => {
-          const payment = row.original
-    
-          return (
-            <div className="flex space-x-2">
-             
-              <Button
-                className="cursor-pointer"
-                variant="destructive"
-                size="sm"
-                onClick={() => console.log("Borrar", payment.id)} // Aquí va tu lógica de borrar
-              >
-                Borrar
-              </Button>
-            </div>
-          )
-        },
-    }
-]
+  },
+];
