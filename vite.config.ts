95d2daf3343239ac9de6b7ec5fac8ab9f381fe5a
MatiasBangher <matiasbangher06@gmail.com>
2025-05-08 21:51:16 -0300

    config pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
-import path from "path"
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
